fd8a6b5b46978729ad1275bc0b158057b2b59310
Eesa Gurling <eesagurling@gmail.com>
2024-06-06 20:31:22 +0100

    Basic Layout

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,38 @@
+import './App.scss'
+
 function App() {
+  const loggedInUser = "Eesa";
 
   return (
-    <p>Hello World</p>
+    <div className="layout">
+      <header>
+        <h1>Basic React Demo</h1>
+        <p className="welcome">Welcome {loggedInUser}</p>
+      </header>
+
+      <nav>
+        <div className="navItem">
+          <a to="/">Home</a>
+        </div>
+
+        <div className="navItem">
+          <a to="/modules">Modules</a>
+        </div>
+
+        <div className="navItem">
+          <a to="/students">Students</a>
+        </div>
+      </nav>
+
+      <main>
+        <p>Homepage</p>
+      </main>
+
+      <footer>
+        <p className="thankyou">Thank You For Using This System!</p>
+      </footer>
+
+    </div>
   )
 }
 
