d31d43b5cb543c7f96e12e16413c2e82ff8fc20f
Eesa Gurling <eesagurling@gmail.com>
2024-06-12 22:26:31 +0100

    some fixes/debugs

--- src/components/entity/module/ModuleForm.jsx
+++ src/components/entity/module/ModuleForm.jsx
@@ -8,36 +8,83 @@ const initialModule = {
     ModuleCode: null,
     ModuleLevel: 0,
     ModuleYearID: null,
-    ModuleImageURL: "https://images.freeimages.com/images/small-previews/9b8/electronic-component"
-}
+    ModuleLeaderID: null,
+    ModuleImageURL: "https://images.freeimages.com/images/small-previews/9b8/electronic-component",
+};
 
 function ModuleForm({ onCancel }) {
     // Initialisation --------------------------------------------
+    const conformance = {
+        html2js: {
+            ModuleName: (value) => value === '' ? null : value,
+            ModuleCode: (value) => value === '' ? null : value,
+            ModuleLevel: (value) => parseInt(value),
+            ModuleYearID: (value) => value === 0 ? null : parseInt(value),
+            ModuleLeaderID: (value) => value === 0 ? null : parseInt(value),
+            ModuleImageURL: (value) => value === '' ? null : value,
+        },
+        js2html: {
+            ModuleName: (value) => value === null ? '' : value,
+            ModuleCode: (value) => value === null ? '' : value,
+            ModuleLevel: (value) => value,
+            ModuleYearID: (value) => value === null ? 0 : value,
+            ModuleLeaderID: (value) => value === null ? 0 : value,
+            ModuleImageURL: (value) => value === null ? '' : value,
+        },
+    };
+
     // State -----------------------------------------------------
     const [module, setModule] = useState(initialModule);
     // Handler ---------------------------------------------------
+    const handleChange = (event) => {
+        const { name, value } = event.target;
+        setModule({...module, [name]: conformance.html2js[name](value)});
+    };
+
+    const handleSubmit = () => {
+        console.log(`Module=[${JSON.stringify(module)}]`)
+    };
+
     // View ------------------------------------------------------
     return (
         <div className="moduleForm">
             <div className='FormTray'>
-                <label htmlFor="">Module Name</label>
-                <input type="text" name="ModuleName" value={module.ModuleName} />
+                <label>
+                    Module Name
+                    <input 
+                    type="text" 
+                    id="ModuleName" 
+                    name="ModuleName" 
+                    value={conformance.js2html["ModuleName"](module.ModuleName)} 
+                    onChange={handleChange}
+                    />
+                </label>
 
-                <label htmlFor="">Module Code</label>
-                <input type="text" name="ModuleCode" value={module.ModuleCode} />
+                <label>
+                    Module Code
+                    <input 
+                    type="text" 
+                    name="ModuleCode" 
+                    value={conformance.js2html["ModuleName"](module.ModuleCode)} 
+                    onChange={handleChange}
+                    />
+                </label>
 
-                <label htmlFor="">Module Level</label>
-                <select name="ModuleLevel" value={module.ModuleLevel}>
+                <label>
+                    Module Level
+                    <select name="ModuleLevel" value={conformance.js2html["ModuleName"](module.ModuleLevel)} onChange={handleChange}>
                     <option value="0" disabled>
                         None slected
                     </option>
                     {[3,4,5,6,7].map((level) => (
                         <option key={level}>{level}</option>
                     ))}
-                </select>
+                    </select>
+                </label>
             </div>
 
             <Action.Tray>
+              <Action.Submit showText onClick={handleSubmit}/>
               <Action.Cancel showText buttonText="Cancel Form" onClick={onCancel}/>
             </Action.Tray>
 
